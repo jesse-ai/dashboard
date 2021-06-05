aeb625b4b43d1ff69949a77880fd93b1bb8bdd48
Nicolay Zlobin <nicolay.zlobin@gmail.com>
2021-06-05 12:16:25 +0300

    Update vue to work with latest babel
    To support latest Tailwind features

--- src/main.js
+++ src/main.js
@@ -5,4 +5,7 @@ import store from './store'
 
 import './assets/styles/styles.css'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+  .use(store)
+  .use(router)
+  .mount('#app')
