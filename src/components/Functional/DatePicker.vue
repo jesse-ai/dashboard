bc8f781cd04f407f8a7e2fb738a33c4e6b96c2d7
Saleh Mir <algo@hey.com>
2021-06-24 23:52:26 +0430

    Update DatePicker.vue

--- src/components/Functional/DatePicker.vue
+++ src/components/Functional/DatePicker.vue
@@ -1,24 +1,20 @@
 <template>
-  <div>
-    <div class="mt-1 relative rounded-md shadow-sm">
-      <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-        <CalendarIcon class="h-5 w-5 text-gray-400" aria-hidden="true"/>
-      </div>
-      <input id="email"
-             type="text"
-             name="email"
-             class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-10 sm:text-sm border-gray-300 rounded-md"
-             placeholder="YYYY-MM-DD">
-    </div>
-  </div>
+  <input :id="name"
+         type="date"
+         :name="name"
+         class="focus:ring-indigo-500 focus:border-indigo-500 flex justify-center items-center w-48 text-center sm:text-sm border-gray-300 rounded-md"
+  >
 </template>
 
 <script>
-import { CalendarIcon } from '@heroicons/vue/solid'
 
 export default {
-  components: {
-    CalendarIcon
-  },
+  name: 'DatePicker',
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  }
 }
 </script>
