403b0502820ca671c92dd58e30a5d5628ffe69ec
Saleh Mir <algo@hey.com>
2021-06-25 00:05:33 +0430

    Update Backtest.vue

--- src/views/Backtest.vue
+++ src/views/Backtest.vue
@@ -1,8 +1,44 @@
 <template>
-  <div class="container">
-    <h1 class="mb-5">Backtest</h1>
+  <!-- Page title & actions -->
+  <div class="border-b border-gray-200 px-4 py-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8">
+    <div class="flex-1 min-w-0">
+      <h1 class="text-lg font-medium leading-6 text-gray-900 sm:truncate">
+        Backtest
+      </h1>
+    </div>
+    <div class="mt-4 flex sm:mt-0 sm:ml-4">
+      <button type="button" class="btn-secondary mr-2">
+        Share
+      </button>
+      <button type="button" class="btn-success">
+        Create
+      </button>
+    </div>
+  </div>
 
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
     <!--    <CircleProgressbar :progress="20"/>-->
+
+    <!--    <div class="my-6 grid grid-cols-4 gap-4">-->
+    <!--      <Select :options="routes.exchanges" />-->
+    <!--      <Select :options="routes.symbols" />-->
+    <!--      <Select :options="routes.timeframes" />-->
+    <!--      <Select :options="routes.strategies" />-->
+    <!--    </div>-->
+
+    <div class="my-6">
+      <div class="flex justify-between items-center">
+        <h3 class="font-semibold mb-2">
+          Routes
+        </h3>
+
+        <div>
+          sdff
+        </div>
+      </div>
+
+      <Route/>
+    </div>
 
     <br>
 
@@ -30,9 +66,9 @@
 
     <div class="flex justify-between items-center">
       <div class="flex items-center select-none">
-        <DatePicker/>
+        <DatePicker class="flex-1" name="start_date"/>
         <span class="mx-2">to</span>
-        <DatePicker/>
+        <DatePicker class="flex-1" name="finish_date"/>
       </div>
 
       <div>
@@ -52,16 +88,28 @@
 import CircleProgressbar from '@/components/Functional/CircleProgressbar'
 import Toggle from '@/components/Functional/Toggle'
 import DatePicker from '@/components/Functional/DatePicker'
+import Select from '@/components/Functional/Select'
+import Route from '@/components/Functional/Route'
+// import { PlusCircleIcon } from ''
 
 export default {
   name: 'Backtest',
   components: {
     CircleProgressbar,
     Toggle,
-    DatePicker
+    DatePicker,
+    Select,
+    Route
   },
   data () {
-    return {}
+    return {
+      routes: {
+        exchanges: ['Binance', 'Binance Futures'],
+        symbols: ['BTC-USDT', 'ETH-USDT'],
+        timeframes: ['1m', '3m', '5m', '15m', '30m', '45m', '1h', '2h', '3h', '4h', '6h', '8h', '12h', '1D', '3D', '1W'],
+        strategies: ['TrendFollowing01']
+      }
+    }
   },
   computed: {},
   mounted () {
