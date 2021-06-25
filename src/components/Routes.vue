e2f7a323b8cd44a055735bd9cafdbfed409f834a
Saleh Mir <algo@hey.com>
2021-06-25 21:20:12 +0430

    add dropdown to routes

--- src/components/Routes.vue
+++ src/components/Routes.vue
@@ -44,7 +44,45 @@
       </select>
 
       <div class="flex flex-col items-center justify-center cursor-pointer hover:bg-gray-50 rounded-r-lg">
-        <DotsVerticalIcon class="py-1 pr-1 mx-5 h-8 w-8 text-gray-400" />
+        <Menu as="div" class="relative block h-full w-full">
+          <MenuButton class="px-5 block text-left h-full w-full focus:outline-none">
+            <DotsVerticalIcon class="h-8 w-8 text-gray-400" />
+          </MenuButton>
+
+          <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75"
+                      leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+            <MenuItems class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white z-10 ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none">
+              <div class="py-1">
+                <MenuItem v-slot="{ active }">
+                  <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'group flex items-center px-4 py-2 text-sm']">
+                    <TrashIcon class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500" aria-hidden="true" />
+                    Delete
+                  </a>
+                </MenuItem>
+                <MenuItem v-slot="{ active }">
+                  <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'group flex items-center px-4 py-2 text-sm']">
+                    <DuplicateIcon class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500" aria-hidden="true" />
+                    Duplicate
+                  </a>
+                </MenuItem>
+              </div>
+              <div class="py-1">
+                <MenuItem v-slot="{ active }">
+                  <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'group flex items-center px-4 py-2 text-sm']">
+                    <ArrowCircleUpIcon class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500" aria-hidden="true" />
+                    Move Up
+                  </a>
+                </MenuItem>
+                <MenuItem v-slot="{ active }">
+                  <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'group flex items-center px-4 py-2 text-sm']">
+                    <ArrowCircleDownIcon class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500" aria-hidden="true" />
+                    Move Down
+                  </a>
+                </MenuItem>
+              </div>
+            </MenuItems>
+          </transition>
+        </Menu>
       </div>
     </div>
   </div>
@@ -52,14 +90,40 @@
 
 
 <script>
-import { PlusSmIcon, DotsVerticalIcon } from '@heroicons/vue/solid'
-
+import {
+  PlusSmIcon, DotsVerticalIcon,
+  ArchiveIcon,
+  ArrowCircleRightIcon,
+  ChevronDownIcon,
+  DuplicateIcon,
+  HeartIcon,
+  PencilAltIcon,
+  TrashIcon,
+  UserAddIcon,
+  ArrowCircleUpIcon,
+  ArrowCircleDownIcon
+} from '@heroicons/vue/solid'
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 
 export default {
   name: 'Routes',
   components: {
     PlusSmIcon,
-    DotsVerticalIcon
+    DotsVerticalIcon,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuItems,
+    ArchiveIcon,
+    ArrowCircleRightIcon,
+    ChevronDownIcon,
+    DuplicateIcon,
+    HeartIcon,
+    PencilAltIcon,
+    TrashIcon,
+    UserAddIcon,
+    ArrowCircleUpIcon,
+    ArrowCircleDownIcon
   },
   props: {
     exchanges: {
