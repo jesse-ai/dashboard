8cdcb5451c9b3ef66cb7509239ccd0afcdf2658e
Saleh Mir <algo@hey.com>
2021-06-25 21:36:06 +0430

    use rounded buttons for divider buttons

--- src/components/Routes.vue
+++ src/components/Routes.vue
@@ -8,10 +8,17 @@
         <span class="pr-3 bg-white text-lg font-medium text-gray-900">
           Routes
         </span>
-        <button type="button" class="inline-flex items-center shadow-sm px-4 py-1.5 border border-gray-300 text-sm leading-5 font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none">
-          <PlusSmIcon class="-ml-1.5 mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
-          <span>Add</span>
-        </button>
+
+        <span>
+          <button type="button" class="inline-flex items-center shadow-sm px-4 py-1.5 border border-gray-300 text-sm leading-5 font-medium rounded-l-full text-gray-700 bg-white hover:bg-gray-50 focus:outline-none">
+            <PlusSmIcon class="-ml-1.5 mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+            <span>Trading Route</span>
+          </button>
+          <button type="button" class="inline-flex items-center shadow-sm px-4 py-1.5 border border-gray-300 text-sm leading-5 font-medium rounded-r-full text-gray-700 bg-white hover:bg-gray-50 focus:outline-none">
+            <PlusSmIcon class="-ml-1.5 mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+            <span>Extra Route</span>
+          </button>
+        </span>
       </div>
     </div>
 
