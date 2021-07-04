480ec01d83115afe06219ff9a6860c13b573a14c
Saleh Mir <algo@hey.com>
2021-07-04 12:45:54 +0430

    refactor layouts

--- src/views/Backtest.vue
+++ src/views/Backtest.vue
@@ -1,47 +1,45 @@
 <template>
-  <section class="h-full flex flex-col justify-between">
-    <!-- Tabs -->
-    <div class="mb-4">
-      <!--      <div class="sm:hidden">-->
-      <!--        <label for="tabs" class="sr-only">Select a tab</label>-->
-      <!--        <select-->
-      <!--          id="tabs" v-model="$route.params." name="tabs" class="block w-full border-0 border-t border-b border-gray-300">-->
-      <!--          <option v-for="tab in tabs" :key="tab.id" :value="tab.id">{{ tab.name }}</option>-->
-      <!--        </select>-->
-      <!--      </div>-->
-      <div class="hidden sm:block">
-        <nav class="relative z-0 rounded-lg shadow flex divide-x divide-gray-200 " aria-label="Tabs">
-          <div v-for="tab in tabs" :key="tab.id"
-               class="relative group min-w-0 flex-1 overflow-hidden text-center flex items-center"
-          >
-            <router-link
-              :class="[tab.id === pageId ? 'text-gray-900 font-bold' : 'text-gray-500 hover:text-gray-700 bg-white', 'py-4 px-4 inline-block select-none cursor-pointer focus:outline-none bg-white w-full text-sm font-medium hover:bg-gray-50 focus:z-10']"
-              :to="`${tab.id}`">
-              <span>{{ tab.id }}</span>
-              <span aria-hidden="true"
-                    :class="[tab.id === pageId ? 'bg-indigo-500' : 'bg-transparent', 'absolute inset-x-0 bottom-0 h-0.5']"/>
-            </router-link>
+  <!-- Tabs -->
+  <div class="mb-4">
+    <!--      <div class="sm:hidden">-->
+    <!--        <label for="tabs" class="sr-only">Select a tab</label>-->
+    <!--        <select-->
+    <!--          id="tabs" v-model="$route.params." name="tabs" class="block w-full border-0 border-t border-b border-gray-300">-->
+    <!--          <option v-for="tab in tabs" :key="tab.id" :value="tab.id">{{ tab.name }}</option>-->
+    <!--        </select>-->
+    <!--      </div>-->
+    <div class="hidden sm:block">
+      <nav class="relative z-0 rounded-lg shadow flex divide-x divide-gray-200 " aria-label="Tabs">
+        <div v-for="tab in tabs" :key="tab.id"
+             class="relative group min-w-0 flex-1 overflow-hidden text-center flex items-center"
+        >
+          <router-link
+            :class="[tab.id === pageId ? 'text-gray-900 font-bold' : 'text-gray-500 hover:text-gray-700 bg-white', 'py-4 px-4 inline-block select-none cursor-pointer focus:outline-none bg-white w-full text-sm font-medium hover:bg-gray-50 focus:z-10']"
+            :to="`${tab.id}`">
+            <span>{{ tab.id }}</span>
+            <span aria-hidden="true"
+                  :class="[tab.id === pageId ? 'bg-indigo-500' : 'bg-transparent', 'absolute inset-x-0 bottom-0 h-0.5']"/>
+          </router-link>
 
-            <!-- Tab close button -->
-            <button v-show="Object.keys(tabs).length > 1" class="absolute right-[1em] focus:outline-none z-40" @click="closeTab(tab.id)">
-              <XIcon class="h-5 w-5 text-gray-400 hover:text-gray-600 hover:bg-gray-200 rounded-full" aria-hidden="true" />
-            </button>
-          </div>
+          <!-- Tab close button -->
+          <button v-show="Object.keys(tabs).length > 1" class="absolute right-[1em] focus:outline-none z-40" @click="closeTab(tab.id)">
+            <XIcon class="h-5 w-5 text-gray-400 hover:text-gray-600 hover:bg-gray-200 rounded-full" aria-hidden="true" />
+          </button>
+        </div>
 
-          <!-- New Tab Button-->
-          <div class="select-none cursor-pointer text-gray-400 hover:text-gray-600 focus:outline-none group relative w-14 overflow-hidden bg-white py-4 px-4 font-medium hover:bg-gray-50 focus:z-10 flex items-center justify-center"
-               @click="addTab">
-            <button class="absolute right-[1em] focus:outline-none">
-              <PlusSmIcon class="h-6 w-6 rounded-full" aria-hidden="true" />
-            </button>
-          </div>
-        </nav>
-      </div>
+        <!-- New Tab Button-->
+        <div class="select-none cursor-pointer text-gray-400 hover:text-gray-600 focus:outline-none group relative w-14 overflow-hidden bg-white py-4 px-4 font-medium hover:bg-gray-50 focus:z-10 flex items-center justify-center"
+             @click="addTab">
+          <button class="absolute right-[1em] focus:outline-none">
+            <PlusSmIcon class="h-6 w-6 rounded-full" aria-hidden="true" />
+          </button>
+        </div>
+      </nav>
     </div>
+  </div>
 
-    <!-- Backtest Tabs -->
-    <BacktestTab v-if="tabs[pageId]" :form="tabs[pageId].form" :results="tabs[pageId].results" />
-  </section>
+  <!-- Backtest Tabs -->
+  <BacktestTab v-if="tabs[pageId]" :form="tabs[pageId].form" :results="tabs[pageId].results" />
 </template>
 
 <script>
