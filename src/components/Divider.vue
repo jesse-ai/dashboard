31748678917b8a969faf81b964a905718dc17932
morteza181 <kooh.morteza@yahoo.com>
2021-07-13 15:50:21 +0430

    fix divider dahsed line in dark mode

--- src/components/Divider.vue
+++ src/components/Divider.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative my-4 select-none">
     <div class="absolute inset-0 flex items-center" aria-hidden="true">
-      <div class="w-full border-t-2 border-dashed border-gray-300" />
+      <div class="w-full border-t-2 border-dashed border-gray-300 dark:border-gray-600" />
     </div>
     <div class="relative flex justify-start">
       <span class="pr-3 bg-white dark:bg-backdrop-dark text-lg font-medium text-gray-900 dark:text-gray-100">
