172f3344c653ffb591baa933232801b5cfa3faae
Saleh Mir <algo@hey.com>
2021-07-13 13:23:44 +0430

    Create StatsBox.vue

---
+++ src/components/StatsBox.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="px-4 py-5 border rounded-lg overflow-hidden sm:p-6">
+    <dt class="text-sm font-medium text-gray-500 truncate">
+      {{ name }}
+    </dt>
+    <dd class="mt-1 text-base font-semibold text-gray-900">
+      {{ value }}
+    </dd>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'StatsBox',
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
