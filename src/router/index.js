aa44401760583962a4bda221650b805c031a0bd3
Saleh Mir <algo@hey.com>
2021-07-13 18:55:26 +0430

    switch router mode from history to hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useMainStore } from '@/stores/main'
 
 // Views
@@ -80,7 +80,8 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
+  mode: 'hash',
   routes
 })
 
