0ce7f3a66110456ffc380ae549106be891eb4904
Saleh Mir <algo@hey.com>
2021-07-14 13:22:21 +0430

    select-none for Boxes and tables

--- src/components/MultipleValuesTable.vue
+++ src/components/MultipleValuesTable.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col">
+  <div class="flex flex-col select-none">
     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
       <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
         <div class="border overflow-hidden sm:rounded">
--- src/components/StatsBox.vue
+++ src/components/StatsBox.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="px-4 py-5 border rounded overflow-hidden sm:p-6">
+  <div class="px-4 py-5 border rounded overflow-hidden sm:p-6 select-none">
     <dt class="text-sm font-medium text-gray-500 truncate">
       {{ name }}
     </dt>
