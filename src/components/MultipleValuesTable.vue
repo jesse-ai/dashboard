a2826fd443ce046149bfffe58de53a256d90ee65
morteza181 <kooh.morteza@yahoo.com>
2021-07-17 10:18:20 +0430

    add dark mode style to multiple value table

--- src/components/MultipleValuesTable.vue
+++ src/components/MultipleValuesTable.vue
@@ -4,9 +4,9 @@
       <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
         <div class="border dark:border-gray-600 overflow-hidden sm:rounded">
           <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-600">
-            <thead v-if="header" class="bg-gray-50 select-none">
+            <thead v-if="header" class="bg-gray-50 dark:bg-backdrop-dark select-none">
               <tr>
-                <th v-for="item in headerItems" :key="item" scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                <th v-for="item in headerItems" :key="item" scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-400 uppercase tracking-wider">
                   {{ item }}
                 </th>
               </tr>
@@ -21,7 +21,7 @@
             </tbody>
           </table>
 
-          <div v-if="!dataItems.length" class="text-center py-4 opacity-30 select-none text-xs">
+          <div v-if="!dataItems.length" class="text-center text-xs dark:bg-gray-700 py-4 opacity-30 select-none">
             Empty List
           </div>
         </div>
