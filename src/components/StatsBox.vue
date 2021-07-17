c1471df36a9763b9d7813b3b49587ee08e4e4b63
morteza181 <kooh.morteza@yahoo.com>
2021-07-17 10:04:42 +0430

    add dark mode to state boxes

--- src/components/StatsBox.vue
+++ src/components/StatsBox.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="px-4 py-5 border rounded overflow-hidden sm:p-6 select-none">
-    <dt class="text-sm font-medium text-gray-500 truncate">
+  <div class="px-4 py-5 border dark:border-gray-600 rounded overflow-hidden sm:p-6 select-none">
+    <dt class="text-sm font-medium text-gray-500 dark:text-gray-400 truncate">
       {{ name }}
     </dt>
-    <dd class="mt-1 text-base font-semibold text-gray-900">
+    <dd class="mt-1 text-base font-semibold text-gray-900 dark:text-gray-100">
       {{ value }}
     </dd>
   </div>
