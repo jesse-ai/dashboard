e41ef2fc0b926881f9b21b1d6ce315ef314b80a1
morteza181 <kooh.morteza@yahoo.com>
2021-07-17 12:06:54 +0430

    modify logs background color in dark mode

--- src/components/Logs.vue
+++ src/components/Logs.vue
@@ -1,7 +1,7 @@
 <template>
   <Divider>Logs</Divider>
 
-  <pre class="whitespace-pre-line rounded border dark:border-gray-600 bg-gray-50 dark:bg-backdrop-dark select-text text-sm dark:text-gray-300 w-full px-4 sm:px-6 py-2"
+  <pre class="whitespace-pre-line rounded border dark:border-gray-600 bg-gray-50 dark:bg-gray-700 select-text text-sm dark:text-gray-300 w-full px-4 sm:px-6 py-2"
        :class="full ? 'max-h-[90%]' : 'max-h-[48em]'" v-html="logs"
   />
 </template>
