f70cccd6385ad783c2ccbc8a05b1763816a2a11e
morteza181 <kooh.morteza@yahoo.com>
2021-07-18 17:12:54 +0430

    add enabled props to toogle button

--- src/components/ToggleButton.vue
+++ src/components/ToggleButton.vue
@@ -5,8 +5,8 @@
       <SwitchLabel as="span" class="font-medium text-gray-700 dark:text-gray-200" passive>{{ title }}</SwitchLabel>
       <SwitchDescription as="span" class="text-gray-500 dark:text-gray-400">{{ description }}</SwitchDescription>
     </span>
-    <Switch v-model="enabled" :class="[enabled ? 'bg-indigo-600 dark:bg-indigo-400' : 'bg-gray-200 dark:bg-gray-600', 'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 dark:ring-offset-0 focus:ring-indigo-500 dark:focus:ring-indigo-300']">
-      <span aria-hidden="true" :class="[enabled ? 'translate-x-5 dark:bg-black' : 'translate-x-0 dark:bg-gray-400', 'bg-white  pointer-events-none inline-block h-5 w-5 rounded-full shadow transform ring-0 transition ease-in-out duration-200']" />
+    <Switch v-model="value" :class="[value ? 'bg-indigo-600 dark:bg-indigo-400' : 'bg-gray-200 dark:bg-gray-600', 'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 dark:ring-offset-0 focus:ring-indigo-500 dark:focus:ring-indigo-300']">
+      <span aria-hidden="true" :class="[value ? 'translate-x-5 dark:bg-black' : 'translate-x-0 dark:bg-gray-400', 'bg-white  pointer-events-none inline-block h-5 w-5 rounded-full shadow transform ring-0 transition ease-in-out duration-200']" />
     </Switch>
   </SwitchGroup>
 </template>
@@ -32,11 +32,10 @@ export default {
       type: String,
       required: true
     },
-  },
-  data () {
-    return {
-      enabled: false
+    value: {
+      type: Boolean,
+      required: true
     }
-  }
+  },
 }
 </script>
