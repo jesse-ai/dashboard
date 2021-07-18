60d1ca22490e32e71f8b47471f29cd16df954bf4
morteza181 <kooh.morteza@yahoo.com>
2021-07-18 17:11:37 +0430

    remove dark mode of exceptions

--- src/components/Exception.vue
+++ src/components/Exception.vue
@@ -3,17 +3,17 @@
     Exception
   </Divider>
 
-  <div class="rounded-md bg-red-50 dark:bg-red-900 p-4 my-4">
+  <div class="rounded-md bg-red-50 p-4 my-4">
     <div class="flex">
       <div class="flex-shrink-0">
-        <XCircleIcon class="h-5 w-5 text-red-400 dark:text-red-300" aria-hidden="true" />
+        <XCircleIcon class="h-5 w-5 text-red-400" aria-hidden="true" />
       </div>
 
       <div class="ml-3">
-        <h3 class="text-base font-medium text-red-800 dark:text-red-200 mb-6" v-text="title"/>
+        <h3 class="text-base font-medium text-red-800 mb-6" v-text="title"/>
 
         <pre v-if="content"
-             class="mt-2 text-sm text-red-700 dark:text-red-300 whitespace-pre-line px-6 py-6 rounded-md border-2 border-dashed border-red-200 dark:border-red-600"
+             class="mt-2 text-sm text-red-700 whitespace-pre-line px-6 py-6 rounded-md border-2 border-dashed border-red-200 "
              v-html="content"/>
       </div>
     </div>
