f8c2c1ef69044377cd119517e1ec955471c8554e
morteza181 <kooh.morteza@yahoo.com>
2021-07-19 10:54:43 +0430

    add checkbox component

--- src/components/CheckBox.vue
+++ src/components/CheckBox.vue
@@ -4,11 +4,12 @@
       <input id="debug_mode" v-model="value"
              name="debug_mode"
              type="checkbox"
-             class="focus:ring-0 h-4 w-4 text-indigo-600 border-gray-300 rounded cursor-pointer">
+             :class="[value ? 'dark:border-gray-600' : 'dark:border-gray-500', 'focus:ring-0 h-4 w-4 text-indigo-600 dark:text-gray-600 dark:bg-gray-600 border-gray-300 focus:ring-offset-0 rounded cursor-pointer']"
+      >
     </div>
     <div class="ml-3 text-sm">
-      <label for="debug_mode" class="font-medium text-gray-700 cursor-pointer">{{ title }}</label>
-      <p class="text-gray-500">{{ description }}</p>
+      <label for="debug_mode" class="font-medium text-gray-700 dark:text-gray-200 cursor-pointer">{{ title }}</label>
+      <p class="text-gray-500 dark:text-gray-400">{{ description }}</p>
     </div>
   </div>
 </template>
