f28321e81a98240d08764822e420fd33fddbee11
Saleh Mir <algo@hey.com>
2021-07-22 19:08:24 +0430

    update style

--- src/components/ToggleButton.vue
+++ src/components/ToggleButton.vue
@@ -2,7 +2,7 @@
   <SwitchGroup as="div" class="w-full flex items-center justify-between mb-3 select-none">
     <span class="flex-grow flex flex-col">
       <SwitchLabel as="span" class="font-medium text-gray-700 dark:text-gray-200" passive>{{ title }}</SwitchLabel>
-      <SwitchDescription as="span" class="text-gray-500 dark:text-gray-400">{{ description }}</SwitchDescription>
+      <SwitchDescription as="span" class="text-sm text-gray-500 dark:text-gray-400">{{ description }}</SwitchDescription>
     </span>
 
     <Switch v-model="object[name]" :class="[object[name] ? 'bg-indigo-600 dark:bg-indigo-400' : 'bg-gray-200 dark:bg-gray-600', 'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none']">
