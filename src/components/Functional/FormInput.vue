1be05198bfba1de1c66b72712bc49e48dfef28ab
Saleh Mir <algo@hey.com>
2021-07-26 19:53:15 +0430

    add support for step

--- src/components/Functional/FormInput.vue
+++ src/components/Functional/FormInput.vue
@@ -3,11 +3,24 @@
     <label
       class="block text-sm font-medium text-gray-700">{{ title }}</label>
 
-    <input v-model="object[name]"
+    <input v-if="inputType === 'number'"
+           v-model="object[name]"
+           :name="name"
+           type="number"
+           :autocomplete="name"
+           :placeholder="placeholder"
+           :step="step"
+           class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" >
+
+    <input v-else
+           v-model="object[name]"
            :name="name"
            :type="inputType"
            :autocomplete="name"
+           :placeholder="placeholder"
            class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" >
+
+    <p v-if="description" class="mt-2 text-sm text-gray-500">{{ description }}</p>
   </div>
 </template>
 
@@ -31,6 +44,18 @@ export default {
     inputType: {
       type: String,
       default: 'text'
+    },
+    description: {
+      type: String,
+      default: ''
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    step: {
+      type: Number,
+      default: 1
     }
   }
 }
