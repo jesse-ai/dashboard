1501dbd02369e09dfd1b6048e7c64c929ff356b7
morteza181 <kooh.morteza@yahoo.com>
2021-09-10 16:08:57 +0430

    add create ticket api

--- src/views/Tickets.vue
+++ src/views/Tickets.vue
@@ -1,12 +1,45 @@
 <template>
   <div class="p-6 mt-4">
+    <CustomModal :object="openNewTicket" name="open">
+      <template #head>
+        Create New Ticket
+      </template>
+
+      <template #main>
+        <label class="font-semibold">Title:</label>
+        <input id="content" v-model="title"
+               placeholder="Title"
+               required
+               name="content"
+               class="input mt-2 focus:ring-indigo-500 focus:border-indigo-500 dark:focus:ring-indigo-400 dark:focus:border-indigo-400 sm:text-sm border border-gray-300" >
+        <br>
+        <label class="font-semibold">Description:</label>
+        <textarea id="content" v-model="description"
+                  placeholder="Inert your ticket..."
+                  required
+                  name="content"
+                  rows="10"
+                  class="dark:bg-gray-800 dark:border-gray-900 mt-2 block w-full focus:ring-indigo-500 focus:border-indigo-500 dark:focus:ring-indigo-400 dark:focus:border-indigo-400 sm:text-sm border border-gray-300 rounded" />
+
+        <br>
+      </template>
+
+      <template #footer>
+        <button type="button" class="btn-primary" :disabled="!description.length || !title.length" @click="createTicket()">
+          Create
+        </button>
+        <button type="button" class="font-medium select-none items-center text-gray-700 dark:text-gray-300 hover:underline focus:outline-none text-base tracking-wide mr-4" @click="openNewTicket.open = false">
+          Cancel
+        </button>
+      </template>
+    </CustomModal>
     <div class="px-6">
       <div class="flex justify-between items-center">
         <h1>
           Tickets
         </h1>
       
-        <button v-if="tickets.length > 0" class="flex items-center focus:outline-none lg:ml-20">
+        <button v-if="tickets.length > 0" class="flex items-center focus:outline-none lg:ml-20" @click="openNewTicket.open = true">
           <div class="mr-1 text-sm text-indigo-600 dark:text-indigo-400">
             Create new ticket 
           </div>
@@ -43,7 +76,7 @@
             You have not created any tickets.
           </div>
 
-          <button class="flex items-center focus:outline-none mt-6">
+          <button class="flex items-center focus:outline-none mt-6" @click="openNewTicket.open = true">
             <div class="mr-1 text-indigo-600 hover:underline dark:text-indigo-400">
               Create new ticket 
             </div>
@@ -59,14 +92,19 @@
 import axios from 'axios'
 import moment from 'moment'
 import { PlusCircleIcon } from '@heroicons/vue/solid'
+import CustomModal from '@/components/Modals/CustomModal'
 
 export default {
   components: {
     PlusCircleIcon,
+    CustomModal
   },
   data () {
     return {
+      title: '',
+      description: '',
       tickets: [],
+      openNewTicket: { open: true }
     }
   },
   created () {
@@ -100,6 +138,22 @@ export default {
           return true
         }
       }
+    },
+    createTicket () {
+      axios.post('/ticket', {
+        description: this.description,
+        title: this.title,
+      }).then((res) => {
+        if (res.data.status === 'success') {
+          this.description = ''
+          this.title = ''
+          this.notyf.success(res.data.message)
+        } else if (res.data.status === 'error') {
+          this.notyf.error(res.data.message)
+        }
+      }).catch(error => {
+        this.notyf.error(`[${error.response.status}]: ${error.response.statusText}`)
+      })
     }
   }
 }
