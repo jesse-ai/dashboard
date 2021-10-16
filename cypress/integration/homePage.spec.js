8ca8b3474790ec6ea09cf66bb1b68f10c8716792
morteza-koohgard <morteza73.koohgard@gmail.com>
2021-10-16 12:32:06 +0330

    work on home page test

--- cypress/integration/homePage.spec.js
+++ cypress/integration/homePage.spec.js
@@ -3,6 +3,9 @@ const loginDetails = require('../fixtures/login')
 describe('test home page', () => {
     beforeEach(() => {
         cy.intercept('post', '/auth', { fixture: 'login.json' }).as('login')
+        cy.intercept('post', '/general-info', { fixture: 'generalInfo.json' }).as('generalInfo')
+        cy.intercept('post', '/get-config', { fixture: 'getConfig.json' }).as('getConfig')
+        cy.intercept('post', '/update-config', { fixture: 'updateConfig.json' }).as('updateConfig')
     })
     it('test content', () => {
         cy.visit('/')
