15d5aad9b6e92303ec8fdbf673d305421b3c486b
morteza-koohgard <morteza73.koohgard@gmail.com>
2021-10-19 12:32:33 +0330

    fix test for log in page

--- cypress/integration/homePage.spec.js
+++ cypress/integration/homePage.spec.js
@@ -1,7 +1,10 @@
-const loginDetails = require('../fixtures/login')
+const { default: axios } = require('axios')
 
 describe('test home page', () => {
     beforeEach(() => {
+        sessionStorage.auth_key = null
+        axios.defaults.headers.common.Authorization = null
+
         cy.intercept('post', '/auth', { fixture: 'login.json' }).as('login')
         cy.intercept('post', '/general-info', { fixture: 'generalInfo.json' }).as('generalInfo')
         cy.intercept('post', '/get-config', { fixture: 'getConfig.json' }).as('getConfig')
@@ -9,8 +12,14 @@ describe('test home page', () => {
     })
     it('test content', () => {
         cy.visit('/')
+        cy.contains('Welcome Back!')
         cy.get('input').type('test')
         cy.get('button').click()
-    })
 
+        cy.wait(100)
+        cy.contains('Routes')
+        cy.contains('Import Candles')
+        cy.contains('Start in a new tab')
+
+    })
 })
