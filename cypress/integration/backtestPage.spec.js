4beeeadc6fe1284bf92870d39dcd40a5797244e9
morteza-koohgard <morteza73.koohgard@gmail.com>
2021-10-19 15:10:07 +0330

    add test for backtest page

--- cypress/integration/backtestPage.spec.js
+++ cypress/integration/backtestPage.spec.js
@@ -2,24 +2,23 @@ const { default: axios } = require('axios')
 
 describe('test home page', () => {
     beforeEach(() => {
-        sessionStorage.auth_key = null
-        axios.defaults.headers.common.Authorization = null
-
         cy.intercept('post', '/auth', { fixture: 'login.json' }).as('login')
         cy.intercept('post', '/general-info', { fixture: 'generalInfo.json' }).as('generalInfo')
         cy.intercept('post', '/get-config', { fixture: 'getConfig.json' }).as('getConfig')
         cy.intercept('post', '/update-config', { fixture: 'updateConfig.json' }).as('updateConfig')
-    })
-    it('test content', () => {
+
+        sessionStorage.auth_key = null
+        axios.defaults.headers.common.Authorization = null
         cy.visit('/')
         cy.contains('Welcome Back!')
         cy.get('input').type('test')
         cy.get('button').click()
-
-        cy.wait(100)
+        cy.wait(50)
+    })
+    it('test backtest page', () => {
+        cy.visit('/#/backtest')
         cy.contains('Routes')
         cy.contains('Import Candles')
         cy.contains('Start in a new tab')
-
     })
 })
