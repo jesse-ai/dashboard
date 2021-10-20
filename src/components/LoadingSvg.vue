0ceb9ddd0303d2a2e370022cbfb9bb242eb5c538
morteza-koohgard <morteza73.koohgard@gmail.com>
2021-10-20 14:57:54 +0330

    add loading svg component

---
+++ src/components/LoadingSvg.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg v-if="object[name]" class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24">
+    <!-- ... -->
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'LoadingSvg',
+  props: {
+    object: {
+      type: Object,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    }
+  }
+}
+</script>
+
