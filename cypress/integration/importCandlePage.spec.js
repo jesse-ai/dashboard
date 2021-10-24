53dedadafee57be39202eb2439ef37f795e51484
morteza-koohgard <morteza73.koohgard@gmail.com>
2021-10-24 19:40:03 +0330

    update import candles

--- cypress/integration/importCandlePage.spec.js
+++ cypress/integration/importCandlePage.spec.js
@@ -6,6 +6,8 @@ describe('test home page', () => {
         cy.intercept('post', '/general-info', { fixture: 'generalInfo.json' }).as('generalInfo')
         cy.intercept('post', '/get-config', { fixture: 'getConfig.json' }).as('getConfig')
         cy.intercept('post', '/update-config', { fixture: 'updateConfig.json' }).as('updateConfig')
+        cy.intercept('post', '/import-candles', { fixture: 'importCandles.json' }).as('importCandles')
+        cy.intercept('delete', '/import-candles', { fixture: 'deleteImportCandles.json' }).as('deleteImportCandles')
 
         sessionStorage.auth_key = null
         axios.defaults.headers.common.Authorization = null
@@ -19,29 +21,29 @@ describe('test home page', () => {
 
     it('test import candles page', () => {
         // test inputs of import candle candles pages
-        cy.get('#candles-exchange').contains('Testnet Binance Futures')
-        cy.get('#candles-exchange').contains('Bitfinex')
-        cy.get('#candles-exchange').select('OKEX')
-        cy.get('#candles-exchange').should('have.value', 'OKEX')
-        cy.get('#candles-symbol').type('BTC-USDT')
-        cy.get('#start-date').should('have.value', '2021-06-01')
+        cy.get('[data-cy="candles-exchange"]').contains('Testnet Binance Futures')
+        cy.get('[data-cy="candles-exchange"]').contains('Bitfinex')
+        cy.get('[data-cy="candles-exchange"]').select('OKEX')
+        cy.get('[data-cy="candles-exchange"]').should('have.value', 'OKEX')
+        cy.get('[data-cy="candles-symbol"]').type('BTC-USDT')
+        cy.get('[data-cy="candles-start-date"]').should('have.value', '2021-06-01')
 
         // test start button
-        cy.get('#start-button').click()
+        cy.get('[data-cy="start-button"]').click()
         cy.wait(50)
         cy.contains('seconds remaining')
-        cy.get('#import-candles-cancel-button').click()
+        cy.get('[data-cy="import-candles-cancel-button"]').click()
         cy.wait(50)
-        cy.contains('Exchange')
+        cy.get('[data-cy="candles-page-content"]').should('include.text', 'Exchange')
 
         // test start in new tab button
-        cy.get('#start-new-tab-button').click()
+        cy.get('[data-cy="start-new-tab-button"]').click()
         cy.wait(50)
-        cy.get('#tab1').click()
+        cy.get('[data-cy="tab1"]').click()
         cy.wait(50)
         cy.contains('seconds remaining')
-        cy.get('#import-candles-cancel-button').click()
+        cy.get('[data-cy="import-candles-cancel-button"]').click()
         cy.wait(50)
-        cy.contains('Start Date')
+        cy.get('[data-cy="candles-page-content"]').should('include.text', 'Start Date')
     })
 })
--- src/views/tabs/CandlesTab.vue
+++ src/views/tabs/CandlesTab.vue
@@ -10,7 +10,7 @@
     <h3 class="mt-8">{{ Math.round(results.progressbar.estimated_remaining_seconds) }} seconds remaining...</h3>
 
     <div class="mt-8">
-      <button id="import-candles-cancel-button" class="btn-secondary w-64" @click="cancel($route.params.id)">
+      <button data-cy="import-candles-cancel-button" class="btn-secondary w-64" @click="cancel($route.params.id)">
         Cancel
       </button>
     </div>
@@ -28,27 +28,27 @@
       </div>
 
       <!-- Content -->
-      <div v-if="!results.executing && !results.showResults"
+      <div v-if="!results.executing && !results.showResults" data-cy="candles-page-content"
            class="px-6 pb-4">
         <Divider>Exchange</Divider>
         <select
-          id="candles-exchange"
           v-model="form.exchange"
+          data-cy="candles-exchange"
           class="dark:bg-backdrop-dark hover:bg-gray-50 dark:hover:bg-gray-800 cursor-pointer w-full px-6 py-6 border border-gray-200 dark:border-gray-900 focus:outline-none focus:ring-0 dark:focus:border-indigo-400 focus:border-indigo-500 rounded-md">
           <option v-for="item in routes.exchanges" :key="item">{{ item }}</option>
         </select>
 
         <Divider class="mt-16">Symbol</Divider>
-        <input id="candles-symbol"
-               v-model="form.symbol"
+        <input v-model="form.symbol"
+               data-cy="candles-symbol"
                placeholder="ex: BTC-USDT"
                class="dark:bg-backdrop-dark hover:bg-gray-50 dark:hover:bg-gray-800 w-full px-6 py-6 border border-gray-200 focus:outline-none focus:ring-0 dark:focus:border-indigo-400 focus:border-indigo-500 dark:border-gray-900 rounded-md"
                @input="form.symbol = $event.target.value.toUpperCase()">
 
         <Divider class="mt-16">Start Date</Divider>
         <div class="flex items-center select-none flex-1">
-          <input id="start-date"
-                 v-model="form.start_date"
+          <input v-model="form.start_date"
+                 data-cy="candles-start-date"
                  type="date"
                  name="start_date"
                  class="dark:hover:bg-gray-800 hover:bg-gray-50 cursor-pointer dark:bg-backdrop-dark flex-1 focus:outline-none focus:ring-0 dark:focus:border-indigo-400 focus:border-indigo-500 flex justify-center items-center py-6 text-center sm:text-sm border border-gray-200 dark:border-gray-900 rounded-md"
@@ -71,11 +71,11 @@
         </div>
 
         <div v-else>
-          <button id="start-button" class="btn-primary text-center block w-full mb-4" @click="start($route.params.id)">
+          <button data-cy="start-button" class="btn-primary text-center block w-full mb-4" @click="start($route.params.id)">
             Start
           </button>
 
-          <button id="start-new-tab-button" class="btn-secondary text-center block w-full mb-4" @click="startInNewTab($route.params.id)">
+          <button data-cy="start-new-tab-button" class="btn-secondary text-center block w-full mb-4" @click="startInNewTab($route.params.id)">
             Start in a new tab
           </button>
         </div>
