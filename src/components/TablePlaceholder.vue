74d3551b09b9b93d429ef4deae14a49992dae95d
Saleh Mir <algo@hey.com>
2021-11-04 09:46:05 +0100

    update style for dark mode

--- src/components/TablePlaceholder.vue
+++ src/components/TablePlaceholder.vue
@@ -1,34 +1,34 @@
 <template>
-  <div class="border border-blue-300 rounded-md p-4 w-full mx-auto">
+  <div class="border border-gray-300 dark:border-gray-700 rounded-md p-4 w-full mx-auto">
     <div class="animate-pulse flex space-x-4 mb-8">
       <div class="flex-1 space-y-4 py-1">
-        <div class="h-4 bg-gray-300 rounded w-3/4"/>
+        <div class="h-4 bg-gray-300 dark:bg-gray-700 rounded w-3/4"/>
         <div class="space-y-2">
-          <div class="h-4 bg-gray-300 rounded"/>
-          <div class="h-4 bg-gray-300 rounded w-5/6"/>
+          <div class="h-4 bg-gray-300 dark:bg-gray-700 rounded"/>
+          <div class="h-4 bg-gray-300 dark:bg-gray-700 rounded w-5/6"/>
         </div>
       </div>
       <div class="flex-1 space-y-4 py-1">
-        <div class="h-4 bg-gray-300 rounded w-3/4"/>
+        <div class="h-4 bg-gray-300 dark:bg-gray-700 rounded w-3/4"/>
         <div class="space-y-2">
-          <div class="h-4 bg-gray-300 rounded"/>
-          <div class="h-4 bg-gray-300 rounded w-5/6"/>
+          <div class="h-4 bg-gray-300 dark:bg-gray-700 rounded"/>
+          <div class="h-4 bg-gray-300 dark:bg-gray-700 rounded w-5/6"/>
         </div>
       </div>
     </div>
     <div class="animate-pulse flex space-x-4">
       <div class="flex-1 space-y-4 py-1">
-        <div class="h-4 bg-gray-300 rounded w-3/4"/>
+        <div class="h-4 bg-gray-300 dark:bg-gray-700 rounded w-3/4"/>
         <div class="space-y-2">
-          <div class="h-4 bg-gray-300 rounded"/>
-          <div class="h-4 bg-gray-300 rounded w-2/6"/>
+          <div class="h-4 bg-gray-300 dark:bg-gray-700 rounded"/>
+          <div class="h-4 bg-gray-300 dark:bg-gray-700 rounded w-2/6"/>
         </div>
       </div>
       <div class="flex-1 space-y-4 py-1">
-        <div class="h-4 bg-gray-300 rounded w-3/4"/>
+        <div class="h-4 bg-gray-300 dark:bg-gray-700 rounded w-3/4"/>
         <div class="space-y-2">
-          <div class="h-4 bg-gray-300 rounded"/>
-          <div class="h-4 bg-gray-300 rounded w-4/6"/>
+          <div class="h-4 bg-gray-300 dark:bg-gray-700 rounded"/>
+          <div class="h-4 bg-gray-300 dark:bg-gray-700 rounded w-4/6"/>
         </div>
       </div>
     </div>
