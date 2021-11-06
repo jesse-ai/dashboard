1c4c3196eb43ef6f432690b6ad27ef0ca8034184
morteza-koohgard <morteza73.koohgard@gmail.com>
2021-11-06 16:21:34 +0330

    complete optimization slide button icons

--- src/views/tabs/OptimizationTab.vue
+++ src/views/tabs/OptimizationTab.vue
@@ -134,11 +134,13 @@
       <!-- Action Buttons -->
       <div v-if="!results.executing">
         <div v-if="results.showResults">
-          <button class="btn-primary text-center block mb-4 w-full" @click="rerun($route.params.id)">
+          <button class="flex justify-center items-center btn-primary text-center mb-4 w-full" @click="rerun($route.params.id)">
+            <RefreshIcon class="w-5 h-5 mr-2" />
             Rerun
           </button>
 
-          <button class="btn-success text-center block mb-4 w-full" @click="newOptimization($route.params.id)">
+          <button class="flex justify-center items-center btn-success text-center mb-4 w-full" @click="newOptimization($route.params.id)">
+            <DocumentAddIcon class="w-5 h-5 mr-2" />
             New session
           </button>
 
@@ -215,7 +217,7 @@ import Logs from '@/components/Logs'
 import LayoutWithSidebar from '@/layouts/LayoutWithSidebar'
 import MultipleValuesTable from '@/components/MultipleValuesTable'
 import { useMainStore } from '@/stores/main'
-import { ClipboardIcon, CheckIcon } from '@heroicons/vue/solid'
+import { ClipboardIcon, CheckIcon, RefreshIcon, DocumentAddIcon } from '@heroicons/vue/solid'
 import SlideOver from '@/components/Functional/SlideOver'
 import ToggleButton from '@/components/ToggleButton'
 import Routes from '@/components/Routes'
@@ -238,7 +240,9 @@ export default {
     Divider,
     CircleProgressbar,
     Exception,
-    PlayIcon
+    PlayIcon,
+    RefreshIcon,
+    DocumentAddIcon
   },
   props: {
     form: {
