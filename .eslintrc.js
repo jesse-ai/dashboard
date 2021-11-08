e2c01d6c9d2042cf60a090cdc44f3e4bdda6b907
Saleh Mir <algo@hey.com>
2021-11-08 12:26:35 +0330

    install @vue/cli-plugin-unit-jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,19 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
+
   extends: [
     'plugin:vue/vue3-recommended',
     '@vue/standard'
   ],
+
   parserOptions: {
     parser: 'babel-eslint'
   },
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
@@ -40,5 +44,17 @@ module.exports = {
     'no-extra-semi': 'warn',
     'vue/no-mutating-props': 'off',
     'vue/no-v-html': 'off'
-  }
+  },
+
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
 }
