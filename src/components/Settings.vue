f2e0ca0525e8a02f9d5e384f485d747054211e13
morteza-koohgard <morteza73.koohgard@gmail.com>
2021-11-20 16:15:49 +0330

    remove 1W and 3D time frame from settings component

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -248,7 +248,7 @@ export default {
   },
   data () {
     return {
-      timeframes: ['1m', '3m', '5m', '15m', '30m', '45m', '1h', '2h', '3h', '4h', '6h', '8h', '12h', '1D', '3D', '1W'],
+      timeframes: ['1m', '3m', '5m', '15m', '30m', '45m', '1h', '2h', '3h', '4h', '6h', '8h', '12h', '1D'],
       navigation: [
         { name: 'Backtest', icon: CalculatorIcon },
         { name: 'Optimization', icon: ChipIcon },
