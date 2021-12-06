5326f9115d4aa35aa75dd2fe4a4c1339b6527e42
Saleh Mir <algo@hey.com>
2021-12-06 13:20:13 +0330

    Create FormTextarea.vue

---
+++ src/components/Functional/FormTextarea.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="col-span-6 sm:col-span-3">
+    <label v-if="title" class="input-label">{{ title }}</label>
+
+    <textarea :id="name"
+              v-model="object[name]"
+              :name="name"
+              :rows="rows"
+              class="mt-1 input"
+              :placeholder="placeholder" />
+
+    <p v-if="description" class="mt-2 text-sm text-gray-500">{{ description }}</p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'FormTextarea',
+  props: {
+    title: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    object: {
+      type: Object,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      default: ''
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    rows: {
+      type: Number,
+      required: false,
+      default: 8
+    }
+  }
+}
+</script>
