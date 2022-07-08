09338849957a2b1a05b8708d7ae58503bc7f00be
Saleh Mir <algo@hey.com>
2022-07-08 18:49:06 +0200

    increase the font size of logs in live mode

--- src/components/Logs.vue
+++ src/components/Logs.vue
@@ -1,7 +1,7 @@
 <template>
   <pre
     v-if="logs.length"
-    class="whitespace-pre-line rounded border dark:border-gray-600 bg-gray-50 dark:bg-gray-700 select-text text-sm dark:text-gray-300 w-full px-4 sm:px-6 py-2"
+    class="whitespace-pre-line rounded border dark:border-gray-600 bg-gray-50 dark:bg-gray-700 select-text text-base dark:text-gray-300 w-full px-4 sm:px-6 py-2"
     v-html="logs"
   />
 
