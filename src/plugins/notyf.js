fda644d974994b2febb86c48942e43be53194574
Saleh Mir <algo@hey.com>
2022-09-16 16:26:03 +0200

    change position of notifications to bottom left

--- src/plugins/notyf.js
+++ src/plugins/notyf.js
@@ -2,6 +2,7 @@ import { Notyf } from 'notyf'
 
 export default {
   install: (app, settings) => {
+    settings.position.x = 'left'
     app.config.globalProperties.notyf = new Notyf(settings)
     app.notyf = app.config.globalProperties.notyf
   }
