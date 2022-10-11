2fa49fb408e242d11a36a163cda817a6ce0ee84a
Saleh Mir <algo@hey.com>
2022-10-11 09:46:35 +0200

    Create FullPageLoading.vue

---
+++ src/components/FullPageLoading.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-gray-100 opacity-75 flex flex-col items-center justify-center">
+    <h2 class="text-center text-gray-900 text-xl font-semibold mb-8">Loading...</h2>
+
+    <p class="w-1/3 text-center text-gray-900">This may take a few seconds, please wait</p>
+  </div>
+</template>
